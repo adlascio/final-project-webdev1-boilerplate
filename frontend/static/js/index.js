75aaaebb31b03df7b95875e9f1fe9d7138da2837
Megumi Akama <kamayatu91@gmail.com>
2022-05-18 14:58:09 -0700

    Add dark mode in nav bar

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -3,3 +3,16 @@ import {getCountries} from './countries.js'
 const countries = getCountries();
 
 console.log(countries);
+
+
+//Switch screen mode (light/dark)
+function switchScreenMode() {
+  let screen = document.body;
+  let toggleBtn = document.getElementById("switch");
+
+  toggleBtn.addEventListener("click", function(){
+  screen.classList.toggle("dark");
+  });
+
+}
+switchScreenMode();
--- frontend/static/js/views/Homepage.js
+++ frontend/static/js/views/Homepage.js
@@ -11,7 +11,7 @@ export default class extends AbstractView{
         <div id="home-container">
         <h1 class="home-heading">Where in the world?</h1>
         <img src="static/images/home-main-sm.jpg" class="home-photo" alt="Home photo">
-        <button class="home-btn-search"><i class="fa-solid fa-magnifying-glass"></i> Start Exploring!</button>
+        <a href="/countries" class="home-btn-search"><i class="fa-solid fa-magnifying-glass"></i> Start Exploring!</a>
         </div>`
     }
 }
